4818427bf297db2e719af7ed0810d258931ab62f
Ward Aboroken <wardaboroken@gmail.com>
2025-06-13 09:44:33 +0300

    first commit

---
+++ data/url.js
@@ -0,0 +1,10 @@
+export const baseURL = "https://www.saucedemo.com";
+
+export const url = {
+  loginPage: `${baseURL}/`,
+  inventoryPage: `${baseURL}/inventory.html`,
+  cartPage: `${baseURL}/cart.html`,
+  checkoutStepOne: `${baseURL}/checkout-step-one.html`,
+  checkoutStepTwo: `${baseURL}/checkout-step-two.html`,
+  checkoutComplete: `${baseURL}/checkout-complete.html`,
+};
---
+++ tests/loginTests.spec.js
@@ -0,0 +1,4 @@
+import { test, expect } from "@playwright/test";
+import { SwagLabsPage } from "../pages/swagLabsPage";
+import { usersData } from "../data/usersData";
+import { url } from "../data/url";
